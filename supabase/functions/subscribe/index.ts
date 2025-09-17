4f411720ddbb27bf1d5cefb952c2ca88a2fb9a47
colinr20002000-gif <colinr20002000@gmail.com>
2025-09-17 23:21:30 +0100

    V38D

--- supabase/functions/subscribe/index.ts
+++ supabase/functions/subscribe/index.ts
@@ -7,7 +7,8 @@ Deno.serve(async (req) => {
     return new Response('ok', {
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type'
+        'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
       }
     });
   }
